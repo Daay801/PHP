ce9006aed5503e91af5ff35ea2dcb175fabd10f8
Daay801 <129926476+Daay801@users.noreply.github.com>
2024-04-10 10:15:07 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,11 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+    </body>
+</html>
+PHP程式碼 <?php echo "Hello world!"; ?> 會在每個 h1 標題下方輸出 "Hello world!"。因此，這個網頁會顯示三個 "Hello world!"
